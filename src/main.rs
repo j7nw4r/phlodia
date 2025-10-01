d0b6afb6573d839d4dfbad46ee9ab20fc09513f5
Johnathan W <j7n.w4r@gmail.com>
2025-10-01 17:13:22 -0400

    Basic Cli.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+
+/// Phlodia is a Kubernetes and Cloud Native Kafka distribution.
+#[derive(Parser)]
+pub struct Cli {
+
+}
+
+#[tokio::main]
+async fn main() {
+    let _cli = Cli::parse();
+
+    phlodia::run().await;
 }
